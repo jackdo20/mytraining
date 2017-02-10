4c91ba5d16fede32646c9778f03c0bca76c745ae
jackdo20 <jackdo20@gmail.com>
2017-02-10 23:09:34 +0900

    repeat training

---
+++ python/repeat.py
@@ -0,0 +1,24 @@
+'''name = ['Micheal','Bob','Luther']
+for i in name:
+	print(i)
+'''
+numbers=[]
+i=0
+nums=int(input('how many numbers you want?'))
+summ=0
+while i<nums:
+    print('input number %d: ' % i)
+    numbers.append(input())
+    i=i+1
+
+print('-------------')
+print('your number is:')
+print(numbers)
+print('-------------')
+print('their sum is:')
+
+#print(type(numbers[1]))
+for el in numbers:
+    summ=summ+int(el)
+
+print(summ)
